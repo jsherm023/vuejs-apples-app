0a547fb942aef895b49c5930c8c58e56f06d1812
“Joe <joseph.sherman@Loop.colum.edu>
2019-01-28 18:35:33 -0600

    Add bootstrap and styling. Add nav bar to App.vue and containers to home.vue.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <p v-for="apple in apples"> {{apple.variety}}</p>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4" v-for="apple in apples">
+          <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">Variety: {{ apple.variety}}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="card-link">Card link</a>
+              <a href="#" class="card-link">Another link</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
